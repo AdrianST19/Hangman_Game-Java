0a6c5f23cb4fa21a66019d6b9e4121da14c148c5
Adrian Stefan <92085719+AdrianST19@users.noreply.github.com>
2021-11-20 20:26:29 +0200

    Added file

--- App.java
+++ App.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class App {
+public class Spanzuratoarea {
   public static void main(String[] args) throws FileNotFoundException {  
     
     Scanner input = new Scanner(System.in);
